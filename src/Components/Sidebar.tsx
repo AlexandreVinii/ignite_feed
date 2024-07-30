1c18ceaf4ec3e57bb1dd2033e35a3757a9e829d7
Alex Dev <alexandreviniciusfs@gmail.com>
2024-07-29 21:24:54 -0300

    sidebar ajuste

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 interface SidebarProps {
@@ -8,16 +9,21 @@ export const Sidebar: React.FC<SidebarProps> = () => {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover}
-                src="https://images.unsplash.com/photo-1678055703308-42e919de802c?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                src="https://images.unsplash.com/photo-1678055703308-42e919de802c?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/AlexandreVinii.png" />
+
                 <strong>Alexandre Vinicius</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu Perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu Perfil
+                </a>
             </footer>
         </aside>
     );
